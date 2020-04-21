62976bdb6d62aff50d63ecc3aec28fe2a8149be5
jemychen <jemychen@live.com>
2020-04-21 16:40:18 -0400

    Reached up to delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+let priority = [red, yellow, green]
+
+let taskList = document.querySelector("#tasks")
+
+const newTask = document.querySelector("#create-task-form")
+
+newTask.addEventListener("submit", (e) => {
+  e.preventDefault()
+  // debugger
+  let ourForm = e.target
+  let taskInput = ourForm["new-task-description"]
+  let taskStr = taskInput.value
+
+  takeNewTask(taskStr)
+
+})
+
+function takeNewTask(aTaskStr) {
+    let taskLi = document.createElement("li")
+    taskLi.innerText = aTaskStr
+    taskList.append(taskLi)
+    
+    taskLi.addEventListener("click", () => {
+      taskLi.remove()
+    })
+}
